8dd72413fcc2b84c6eedf49d3cc499c3d2cc44dc
Yason Mogutin <yason4ik87@gmail.com>
2016-06-08 17:33:31 +0300

    final_commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,23 @@
 class User < ActiveRecord::Base
 	has_attached_file :avatar, 
-:path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
-:url => "/system/:attachment/:id/:basename_:style.:extension",
-:styles => {
-  :thumb    => ['100x100#',  :jpg, :quality => 70],
-  :preview  => ['480x480#',  :jpg, :quality => 70],
-  :large    => ['600>',      :jpg, :quality => 70],
-  :retina   => ['1200>',     :jpg, :quality => 30]
-},
-:convert_options => {
-  :thumb    => '-set colorspace sRGB -strip',
-  :preview  => '-set colorspace sRGB -strip',
-  :large    => '-set colorspace sRGB -strip',
-  :retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
-}
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+       :path => ":rails_root/public/system/:attachment/:id/:basename_:style.:extension",
+       :url => "/system/:attachment/:id/:basename_:style.:extension",
+       :styles => {
+                   :thumb    => ['100x100#',  :jpg, :quality => 70],
+                   :preview  => ['480x480#',  :jpg, :quality => 70],
+                   :large    => ['600>',      :jpg, :quality => 70],
+                   :retina   => ['1200>',     :jpg, :quality => 30]
+                   },
+       :convert_options => {
+                            :thumb    => '-set colorspace sRGB -strip',
+                            :preview  => '-set colorspace sRGB -strip',
+                            :large    => '-set colorspace sRGB -strip',
+                            :retina   => '-set colorspace sRGB -strip -sharpen 0x0.5'
+                            }
+    validates_attachment :avatar,
+        :presence => true,
+        :size => { :in => 0..10.megabytes },
+        :content_type => { :content_type => /^image\/(jpeg|png|gif|tiff)$/ }
 	def self.search(search, page)
         paginate :per_page => 5, :page => page,
            :conditions => ['name like ?', "%#{search}%"],
